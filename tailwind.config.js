af4507c0bee5c338d5fddb1d6b8e682ce26c728f
43premansh <42premansh@gmail.com>
2025-06-23 11:47:02 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}"
+  "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  "./app/**/*.{js,ts,jsx,tsx,mdx}", // add if you have an `app/` dir outside src
+  "./components/**/*.{js,ts,jsx,tsx,mdx}", // add if you have a `components/` dir outside src
   ],
   theme: {
     extend: {},
